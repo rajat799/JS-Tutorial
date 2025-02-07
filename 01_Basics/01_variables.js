198583884f9104abd5938600968816f4632f9a75
rajat799 <rajatmalapur@gmail.com>
2025-02-07 09:00:14 +0530

    added 2nd file

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -2,7 +2,18 @@ const accountId = 133456
 let accountEmail = "rajatmala@gmail.com";
 var accountPassword = "12345";
 accountCity = "sangli";
+let accountState;
 
-accountId = 23
+// accountId = 23
 
 console.log(accountId);
+
+accountEmail = "rajat@gmail.com";
+accountPassword = "12345555";
+accountCity = "belgaum";
+
+console.table([accountId ,accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use var: issues in block scope and functional scope
+*/
