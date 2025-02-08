4eb19a1833729b6e44101370022c9ae1bcda91a3
rajat799 <rajatmalapur@gmail.com>
2025-02-08 17:03:40 +0530

    conversion of datatypes

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,42 @@
+// Value conversion
+
+let score = "rajat";
+/*
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinNumber = Number(score);
+console.log(typeof valueinNumber)
+
+console.log(valueinNumber)
+*/
+
+//notes
+// Null - it is an object type = object
+// boolean - it will return 1 or 0 (T OR F)
+// undefined - it will return undefined
+
+// Conversions Rules
+
+// "33" => 33
+// "33svs" => NaN (not an number)
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// note 
+
+// 1 => true, 0=> false
+// "" => false
+// "rajat" => true
+
+
+let someNumer = 33
+
+let stringNumber = String(someNumer)
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
