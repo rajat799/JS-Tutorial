08ae9fbd72ab1e34e5dd1715c24d41a6fb7b22d9
rajat799 <rajatmalapur@gmail.com>
2025-02-08 08:56:20 +0530

    practice_1

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -6,14 +6,27 @@ let accountState;
 
 // accountId = 23
 
-console.log(accountId);
+// console.log(accountId);
 
 accountEmail = "rajat@gmail.com";
 accountPassword = "12345555";
 accountCity = "belgaum";
 
-console.table([accountId ,accountEmail, accountPassword, accountCity, accountState])
+// console.table([accountId ,accountEmail, accountPassword, accountCity, accountState])
 
 /*
 Prefer not to use var: issues in block scope and functional scope
 */
+
+let name = "rajat";
+const EmpId = 12234;
+let EmpSal = 250000;
+
+console.log(EmpId)
+
+
+console.log(name);
+
+console.log(EmpSal);
+
+console.table([name, EmpId, EmpSal]);
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello"); // we are using node js not browser
+
+console.log(6);
+console.log("rajat");
+
+
+// datatypes
+let name = "Rajat";
+let age = 20;
+let isLoggedIN = true;
+
+// Primitive datatypes
+
+// number => 2 to power 53
+//bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => finding uniqueness
+
+//object
+
+console.log(typeof "Rajat");
+
+
