5a76793df4c27d6c868e8eb51596dc677c6eb926
rajat799 <rajatmalapur@gmail.com>
2025-02-11 20:35:48 +0530

    practiced string in test.js

--- test.js
+++ test.js
@@ -1,3 +1,27 @@
-console.log("Rajat");
-console.log("hi how are you");
-console.log("rajat");
+const string1 = "This is string";
+const string2 = 'This is also string';
+const string3 = `yet another string primitive`;
+
+// string constructor (objects)
+const string4 = new String("A string object");
+
+// character access
+console.log("hello");
+
+
+// comparing strings
+
+const a = 1;
+const b = 5;
+
+if(a>b){
+    console.log(`${a} is greater than ${b}`);
+    
+}else if(b>a){
+    console.log(`${b} is greater than ${a}`);
+    
+}
+else{
+    console.log(`${a} and ${b} are equal`);
+    
+}
