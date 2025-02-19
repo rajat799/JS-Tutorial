f5977279b4e9a1b1a5690e2e7529aaae75030c1c
rajat799 <rajatmalapur@gmail.com>
2025-02-19 09:54:37 +0530

    practiced arrays and objects

--- test.js
+++ test.js
@@ -1,25 +1,3 @@
-const newarr = new Array(1, 2, 3, 4,5);
+function addArr(let arr){
 
-console.log(newarr);
-newarr.push(6);
-
-console.log(newarr);
-
-const strarr = newarr.join();
-
-console.log(typeof strarr);
-console.log(strarr);
-
-
-console.log(newarr);
-
-const arr1 = newarr.splice(4)
-console.log(newarr);
-
-console.log(arr1);
-
-
-const arr2 = newarr.slice(2)
-console.log(newarr);
-
-console.log(arr2);
+}
