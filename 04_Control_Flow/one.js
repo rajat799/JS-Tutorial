e2a4a5ebe55c567972328aab1fd57874ec133d16
rajat799 <rajatmalapur@gmail.com>
2025-02-26 22:28:24 +0530

    added control flow folder

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,24 @@
+const isuserLoggedIn = true;
+const temprature = 40;
+
+/*
+if (temprature === 40){
+    console.log("Less than 50");
+}
+else {
+    console.log("temp is greater than 50");
+    
+}
+console.log("executed");
+*/
+
+// <, >, <=, >=, ==, !=, ===
+
+
+const score = 200;
+
+if(score > 100){
+    const power = "fly";
+    console.log(`User power: ${power}`);
+}
+console.log(`User power: ${power}`);
