b518ec7aa27ea667aebeccbfc9a03e66be249618
rajat799 <rajatmalapur@gmail.com>
2025-02-28 18:31:51 +0530

    completed while and dowhile loops

---
+++ 05_Iterations/two.js
@@ -0,0 +1,26 @@
+// while loop
+
+
+let i = 0;  // initialization
+// while (i <= 10) {
+//     console.log(`value of i: ${i}`);
+//     i = i + 2;
+// }
+
+let myArray = ['java', 'javascript', 'cpp', 'python'];
+
+let arr = 0;
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1;
+// }
+
+
+// do while loop (prints first then checks the conditions)
+
+let score = 11;
+
+do {
+    console.log(`score is: ${score}`);
+    score++;
+} while (score <= 10);
